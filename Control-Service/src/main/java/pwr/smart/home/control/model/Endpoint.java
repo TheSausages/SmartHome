d28a15d87a9f86ba15482297fac0f359c153a885
kziejlo <kziejlo@gmail.com>
2022-11-20 22:08:19 +0100

    Updated docekr compose and corrections

--- Control-Service/src/main/java/pwr/smart/home/control/model/Endpoint.java
+++ Control-Service/src/main/java/pwr/smart/home/control/model/Endpoint.java
@@ -7,17 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Getter
 public class Endpoint {
-//    AIR_HUMIDIFIER_URL("http://localhost:8082/setTarget"),
-//    AIR_CONDITIONER_URL("http://localhost:8083/setTarget"),
-//    AIR_FILTER_URL("http://localhost:8084/setTarget"),
-//    DATA_SERVICE_URL("http://localhost:8081/api/data");
-//
-//    public final String url;
-//
-//    Endpoint(String url) {
-//        this.url = url;
-//    }
-
     @Value("${data-service.url}")
     private String dataServiceUrl;
 
@@ -31,14 +20,18 @@ public class Endpoint {
     private String airFilterUrl;
 
     public String getAirConditionerUrl() {
-        return airConditionerUrl + "/setTarget";
+        return "http://" + airConditionerUrl + "/setTarget";
     }
 
     public String getAirFilterUrl() {
-        return airFilterUrl + "/setTarget";
+        return "http://" + airFilterUrl + "/setTarget";
     }
 
     public String getAirHumidifierUrl() {
-        return airHumidifierUrl + "/setTarget";
+        return "http://" + airHumidifierUrl + "/setTarget";
+    }
+
+    public String getDataServiceUrl() {
+        return "http://" + dataServiceUrl;
     }
 }
