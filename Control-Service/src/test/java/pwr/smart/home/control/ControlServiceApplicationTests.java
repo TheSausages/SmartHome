425915be7b490bfa20fadd6c361805f294eec71f
kziejlo <kziejlo@gmail.com>
2022-12-02 23:45:44 +0100

    Small integration tests correction

--- Control-Service/src/test/java/pwr/smart/home/control/ControlServiceApplicationTests.java
+++ Control-Service/src/test/java/pwr/smart/home/control/ControlServiceApplicationTests.java
@@ -3,7 +3,6 @@ package pwr.smart.home.control;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ControlServiceApplicationTests {
 
 	@Test
---
+++ Control-Service/src/test/java/pwr/smart/home/control/integration/tests/BaseIntegrationTest.java
@@ -0,0 +1,46 @@
+package pwr.smart.home.control.integration.tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+/**
+ * Base class for integration tests. Creates 2 containers: a database (Postgres) and Keycloak.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "test")
+public abstract class BaseIntegrationTest {
+	private final static Slf4jLogConsumer containerLogger = new Slf4jLogConsumer(LoggerFactory.getLogger(BaseIntegrationTest.class));
+
+	@Autowired
+	protected ObjectMapper mapper;
+
+	@Autowired
+	protected WebApplicationContext context;
+
+	// Use this to make http cals in integration tests
+	protected WebTestClient webTestClient;
+
+	@BeforeEach
+	public void setup() {
+		MockMvc mockMvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity())
+				.build();
+
+		this.webTestClient = MockMvcWebTestClient
+				.bindTo(mockMvc)
+				.build();
+	}
+}
