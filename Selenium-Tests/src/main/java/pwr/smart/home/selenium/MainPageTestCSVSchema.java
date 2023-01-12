3b69b9252109295a2e51eb8a9b3669df7c1b86de
kziej <kziejlo@gmail.com>
2023-01-12 16:41:36 +0100

    Added main page test

--- Selenium-Tests/src/main/java/pwr/smart/home/selenium/MainPageTestCSVSchema.java
+++ Selenium-Tests/src/main/java/pwr/smart/home/selenium/MainPageTestCSVSchema.java
@@ -1,16 +1,15 @@
 package pwr.smart.home.selenium;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@JsonPropertyOrder({"dataServiceStart", "dataServiceEnd", "dataServiceDifference", "renderStart", "renderEnd", "renderDifference"})
+@JsonPropertyOrder({"fullyLoadTime", "timeToTitle", "timeToFirstByteHomeImage", "timeToFirstByteSite"})
+@AllArgsConstructor
 public class MainPageTestCSVSchema {
-    public LocalDateTime fromDate;
-    public Double dataServiceStart;
-    public Double dataServiceEnd;
-    public Double dataServiceDifference;
-    public LocalDateTime renderStart;
-    public LocalDateTime renderEnd;
-    public Double renderDifference;
+    public Long fullyLoadTime;
+    public Long timeToTitle;
+    public Double timeToFirstByteHomeImage;
+    public Double timeToFirstByteSite;
 }
--- Selenium-Tests/src/test/java/pwr/smart/home/selenium/MainPageTest.java
+++ Selenium-Tests/src/test/java/pwr/smart/home/selenium/MainPageTest.java
@@ -1,7 +1,14 @@
 package pwr.smart.home.selenium;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.junit.jupiter.api.Test;
 import pwr.smart.home.selenium.pages.KeycloakLoginPage;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -29,5 +36,29 @@ public class MainPageTest extends BaseSeleniumTest {
                 notNullValue(),
                 not(emptyIterable())
         ));
+
+        long fullyLoadTime = performance.getTiming().getLoadEventEnd() - performance.getTiming().getNavigationStart();
+        Double timeToFirstByteSite = timings.getTimingsForResources("http://frontend/static/js").stream().map(ResourceTimings.PerformanceResourceTiming::getResponseStart).min(Double::compareTo).get();
+        Double timeToFirstByteImage = timings.getTimingsForResources("home.jpg").get(0).getResponseStart();
+        long timeToTitle = performance.getTiming().getResponseEnd();
+
+        CsvMapper mapper = new CsvMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        try(FileOutputStream strW = new FileOutputStream("./main-page-times.csv")) {
+            var seqW = mapper
+                    .writerFor(MainPageTestCSVSchema.class)
+                    .with(mapper.schemaFor(MainPageTestCSVSchema.class).withHeader())
+                    .writeValues(strW);
+
+            seqW.write(new MainPageTestCSVSchema(
+                    fullyLoadTime,
+                    timeToTitle,
+                    timeToFirstByteImage,
+                    timeToFirstByteSite
+            ));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
